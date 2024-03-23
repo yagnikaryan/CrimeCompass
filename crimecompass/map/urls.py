c6ffa11d8727b661ad5fc81263615bd4411ba1e1
Yagnik Panguluri <yagnikpanguluri@Yagniks-MacBook-Pro.local>
2024-03-23 14:51:18 -0400

    basic html + routes

--- crimecompass/map/urls.py
+++ crimecompass/map/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("map/", views.map, name="map"),
+    path("home/", views.home, name="home")
 ]
--- crimecompass/map/views.py
+++ crimecompass/map/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 
-def index(request):
-    template = loader.get_template('index.html')
+def map(request):
+    template = loader.get_template('map.html')
+    return HttpResponse(template.render())
+
+def home(request):
+    template = loader.get_template('home.html')
     return HttpResponse(template.render())
